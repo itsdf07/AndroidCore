848d9346d90884566cabcc33845fa20da7211b62
itsdf07 <923255742@qq.com>
2020-05-30 10:00:48 +0800

    第一步：初步搭建的网络请求核心库(目标是要基于Retrofit2进行二次封装)

---
+++ lib-core/src/main/java/com/lib/core/net/ConfigKeys.java
@@ -0,0 +1,28 @@
+package com.lib.core.net;
+
+/**
+ * @Description: 定义统一配置全局相关信息的内容 Key （Net框架全局配置内容，当全局内容不变时，使用enum进行定义是最安全的）
+ * 用在配置Configurator中CONFIGS的存储信息
+ * @Author itsdf07
+ * @E-Mail 923255742@qq.com
+ * @Github https://github.com/itsdf07
+ * @Date 2020/5/30
+ */
+public enum ConfigKeys {
+    /**
+     * 接口主机,如http://192.168.3.3:8080/
+     */
+    API_HOST,
+    /**
+     * 全局上下文
+     */
+    APPLICATION_CONTEXT,
+    /**
+     * 拦截器
+     */
+    INTERCEPTOR,
+    /**
+     * 初始化开始时标记为false，配置完成之后需标注为true，标明已经配置好全局信息，之后才能实现其他流程
+     */
+    CONFIG_READY
+}
